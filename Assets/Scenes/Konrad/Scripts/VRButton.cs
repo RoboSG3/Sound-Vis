217ba95c5e2255074125c8bdf7560bca99a93bc4
xxPigelxx <129988942+xxPigelxx@users.noreply.github.com>
2025-01-21 12:04:56 +0100

    Test Wasser(Brocken)

--- Assets/Scenes/Konrad/Scripts/VRButton.cs
+++ Assets/Scenes/Konrad/Scripts/VRButton.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
@@ -7,22 +8,24 @@ public class VRButton : MonoBehaviour
     public float deadTime = 1.0f;
     
     private bool _deadTimeActive = false;
+    
+    public string _compareTag = "Button";
 
     public UnityEvent onPressed, onReleased;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Button") && !_deadTimeActive)
+        if (other.CompareTag(_compareTag) && !_deadTimeActive)
         {
             onPressed?.Invoke();
-            Debug.Log("Pressed");
+            Debug.Log("Pressed "+_compareTag);
         }
     }
 
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Button") && !_deadTimeActive)
+        if (other.CompareTag(_compareTag) && !_deadTimeActive)
         {
             onReleased?.Invoke();
             Debug.Log("Released");
